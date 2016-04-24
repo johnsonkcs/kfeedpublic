36b2366898f4debe1a3f09bf48de1a82e248bf4a
Danial <danialzainul@hotmail.com>
2016-04-24 11:25:20 +0800

    added carrierwave and minimagick for user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,15 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   before_action :authenticate_user!
+
+  before_filter :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:sign_up) do |u| u.permit(:name, :email, :password) 
+    end
+    devise_parameter_sanitizer.for(:account_update) do |u| u.permit(:name, :email, :password, :profile_pic) 
+    end
+  end  
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,9 +5,10 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable,
          :omniauthable, :omniauth_providers => [:facebook]
 
+  mount_uploader :profile_pic, ProfilePicUploader
+
   def self.from_omniauth(auth)
       where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
-        byebug
         user.provider = auth.provider
         user.uid = auth.uid
         user.email = auth.info.email
