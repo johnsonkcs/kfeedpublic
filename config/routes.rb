68271f86a051873d28677f8382eaa3c38e3509a9
Danial <danialzainul@hotmail.com>
2016-04-24 21:00:08 +0800

    added user routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,15 @@ Rails.application.routes.draw do
   resources :feeders
   resources :feedings
   resources :users
+
+  resources :users do 
+    resources :feedings
+  end
+
+  resources :users do 
+    resources :feeders
+  end
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
