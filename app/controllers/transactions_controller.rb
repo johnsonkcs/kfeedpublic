384c3f737275e840ac27ac8455067c11ae20c34e
Yazed Jamal <muyaszed@gmail.com>
2016-04-25 10:22:26 +0800

    complete braintree

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -4,14 +4,15 @@ class TransactionsController < ApplicationController
     def create
       @card_info = params[:save_record]
 
-    	@Feeding = Feeding.find(params[:feeding_id])
+    	@feeding = Feeding.find(params[:feeding_id])
     	@result = Braintree::Transaction.sale(
-              amount: @Feeding.price,
+              amount: @feeding.price,
               payment_method_nonce: params[:payment_method_nonce])
     if @result.success?
     	payment_record 
       redirect_to feeding_path(@feeding), notice: "Congraulations! Your transaction has been successfully!"
     else
+      payment_record
       flash[:alert] = "Something went wrong while processing your transaction. Please try again!"
       gon.client_token = generate_client_token
       render :new
@@ -23,6 +24,8 @@ class TransactionsController < ApplicationController
   Braintree::ClientToken.generate
 end
 def payment_record
+
+  
   if @card_info == "1"
     # PaymentsController.create
      @payment = Payment.create(feeder_id: params[:feeder_id], braintree_id: @result.transaction.id, status: @result.transaction.status, last_4: @result.transaction.credit_card_details.last_4)
