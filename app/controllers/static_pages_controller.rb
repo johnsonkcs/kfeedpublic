9e43b2febd70ea47f51727a6d3eba6a6af09c63b
lenafong <lenafongfs@gmail.com>
2016-04-25 22:46:42 +0800

    created ranking for feeders and feedings

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,5 +2,23 @@ class StaticPagesController < ApplicationController
 
 	def home
 	end
+
+
+	def ranking
+		@user = User.all
+		feeder_record = {}
+		feeding_record = {}
+		@user.each do |user|
+			feeder_count = user.feeders.where(paystatus: :authorized).count
+			feeder_record[user.email]=feeder_count if feeder_count != 0
+
+			feeding_count = user.feedings.count
+			feeding_record[user.email]=feeding_count if feeding_count != 0
+		end
+
+		@feeder_ranking = feeder_record.sort_by {|k,v| -v}.first 10
+		@feeding_ranking = feeding_record.sort_by {|k,v| -v}.first 10
+
+	end 
 	
 end
--- config/routes.rb
+++ config/routes.rb
@@ -19,6 +19,8 @@ Rails.application.routes.draw do
     resources :feeders
   end
 
+
+  get 'ranking' => 'static_pages#ranking'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
