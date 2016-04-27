ff09d600cfd9ff8089eb74051da2f7d99c4a1c97
johnsonkcs <johnsonkcs@gmail.com>
2016-04-27 20:44:18 +0800

    commented out mailing

--- app/controllers/feedings_controller.rb
+++ app/controllers/feedings_controller.rb
@@ -45,14 +45,14 @@ class FeedingsController < ApplicationController
   def create
     @user = current_user
     @feeding = @user.feedings.new(feeding_params)
-    @mailing_list = User.where(:email => "johnson.raz.kev@gmail.com")
+    # @mailing_list = User.where(:email => "johnson.raz.kev@gmail.com")
 
 
     respond_to do |format|
       if @feeding.save
 
-        @mailing_list.each do |subscriber|
-        FeedingsMailer.notification_email(subscriber, @feeding).deliver
+        # @mailing_list.each do |subscriber|
+        # FeedingsMailer.notification_email(subscriber, @feeding).deliver
         end 
 
         format.html { redirect_to @feeding, notice: 'Feeding was successfully created.' }
