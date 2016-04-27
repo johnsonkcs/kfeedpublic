26ce255316104a37ff71f54577864ba905157b42
Yazed Jamal <muyaszed@gmail.com>
2016-04-27 09:29:47 +0800

    finalized landing page

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -26,6 +26,8 @@ $(document).ready(function () {
 
    $("#btn-right").mouseover(function(){
          $(".body-2-left").css("background-color", "white");
+         $(".body-2-right-2").addClass("feeder-bg");
+         
           $(".body-2-right").animate({
             
             right: '0'
@@ -37,6 +39,7 @@ $(document).ready(function () {
    $(".body-2-right").mouseleave(function(){
 
       $(".body-2-left").css("background-color", "#75B9DC");
+      $(".body-2-right-2").removeClass("feeder-bg");
          $(".body-2-right").animate({
             
             right: '-30%'
@@ -48,6 +51,7 @@ $(document).ready(function () {
 
    $("#btn-left").mouseenter(function(){
          $(".body-2-right").css("background-color", "white");
+         $(".body-2-left-1").addClass("org-bg");
           $(".body-2-left").animate({
             left: '0'
         });
@@ -58,6 +62,7 @@ $(document).ready(function () {
 
 
         $(".body-2-right").css("background-color", "#75B9DC");
+        $(".body-2-left-1").removeClass("org-bg");
          $(".body-2-left").animate({
             
             left: '-30%'
